880bfa6b4fac864a4855215d8136e1bd836916b0
Judzhin Miles <info@msbios.com>
2017-12-07 12:17:56 +0200

    Lazy Composer Commiter

---
+++ config/autoload/development.global.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+return [
+    'db' => [
+        'dsn' => 'mysql:dbname=portal.dev;host=127.0.0.1',
+        'username' => 'root',
+        'password' => 'root',
+    ],
+];
